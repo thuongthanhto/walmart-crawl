bade5db4698ed6936766b0d0037132a50ce1ed44
Tô Thành Thương <thuongthanhto@gmail.com>
2021-06-05 01:03:39 +0700

    Done crawl

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const csv = require('csvtojson');
+const queryString = require('query-string');
 
 const csvFilePath = './input.csv';
 
@@ -10,6 +11,9 @@ const chromeOptions = {
 
 (async () => {
   const result = [];
+  let url = '';
+  let tracking_id = '';
+
   const array = await csv().fromFile(csvFilePath);
 
   const browser = await puppeteer.launch(chromeOptions);
@@ -17,30 +21,85 @@ const chromeOptions = {
   await page.goto('https://www.walmart.com/help');
 
   for (const element of array) {
-    await page.waitForTimeout(5000);
+    await page.waitForSelector('button#contact-us');
     await page.click('button#contact-us');
-    await page.waitForTimeout(10000);
+
+    await page.waitForSelector('[aria-label="An order"]');
     await page.click('[aria-label="An order"]');
-    await page.waitForTimeout(10000);
+
+    await page.waitForTimeout(4000);
     await page.type('[aria-label="Type a message"]', element.email, {
       delay: 20,
     });
     await page.click('.wc-send');
-    await page.waitForTimeout(5000);
+
+    await page.waitForTimeout(4000);
     await page.type('[aria-label="Type a message"]', element.fullOrderId, {
       delay: 20,
     });
     await page.click('.wc-send');
+
     await page.waitForTimeout(10000);
-    const status = await page.$eval(
-      'div:nth-child(8) > p',
-      (el) => el.textContent
-    );
+
+    const status = await page.evaluate(() => {
+      const temp = document.querySelector('div:nth-child(8) > p');
+      if (temp) {
+        return temp.textContent;
+      }
+
+      return '';
+    });
+
+    const date = await page.evaluate(() => {
+      const temp = document.querySelector('div:nth-child(11) > p');
+      if (temp) {
+        return temp.textContent;
+      }
+
+      return '';
+    });
+
+    if (await page.$('[aria-label="View Carrier Tracking"]')) {
+      await page.click('[aria-label="View Carrier Tracking"]');
+
+      const getNewPageWhenLoaded = async () => {
+        return new Promise((x) =>
+          browser.on('targetcreated', async (target) => {
+            if (target.type() === 'page') {
+              const newPage = await target.page();
+              const newPagePromise = new Promise((y) =>
+                newPage.once('domcontentloaded', () => y(newPage))
+              );
+              const isPageLoaded = await newPage.evaluate(
+                () => document.readyState
+              );
+              return isPageLoaded.match('complete|interactive')
+                ? x(newPage)
+                : x(newPagePromise);
+            }
+          })
+        );
+      };
+
+      const newPagePromise = getNewPageWhenLoaded();
+      const newPage = await newPagePromise;
+
+      url = newPage.url();
+      await newPage.close();
+
+      const parsed = queryString.parse(
+        url.replace('https://www.walmart.com/tracking', '')
+      );
+      tracking_id = parsed.tracking_id;
+    }
 
     const objectResult = {
       email: element.email,
       fullOrderId: element.fullOrderId,
       status: status,
+      url,
+      date,
+      tracking_id,
     };
     console.log(objectResult);
     result.push(objectResult);
